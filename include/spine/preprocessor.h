779698b098073f4dd481f8d7544f77451df9ef60
saintsHr <hiqkrs@gmail.com>
2026-03-24 14:30:04 -0300

    feat(preprocessor): initial preprocessor

--- include/spine/preprocessor.h
+++ include/spine/preprocessor.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#define MAX_DEFINES             128
+#define MAX_DEFINE_NAME_LENGHT  64
+#define MAX_DEFINE_VALUE_LENGHT 128
+
 char* preprocess(const char* source, long srcSize, const char* filename);
--- src/spine/preprocessor.c
+++ src/spine/preprocessor.c
@@ -2,15 +2,174 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+typedef struct {
+    char name[MAX_DEFINE_NAME_LENGHT];
+    char value[MAX_DEFINE_VALUE_LENGHT];
+} SpDefine;
+
+SpDefine defines[MAX_DEFINES];
+int defineCount = 0;
+
+void parseDefine(char *line) {
+    char name[MAX_DEFINE_NAME_LENGHT];
+    char value[MAX_DEFINE_VALUE_LENGHT];
+
+    char *p = line + 7;
+    while (*p == ' ') p++;
+
+    sscanf(p, "%63s", name);
+
+    char *valueStart = strstr(p, name) + strlen(name);
+    while (*valueStart == ' ') valueStart++;
+
+    strcpy(value, valueStart);
+
+    strcpy(defines[defineCount].name, name);
+    strcpy(defines[defineCount].value, value);
+    defineCount++;
+}
+
+void extractDefines(char *str) {
+    char *read = str;
+    char *write = str;
+    char line[512];
+
+    while (*read) {
+        int i = 0;
+
+        while (*read && *read != '\n') {
+            line[i++] = *read++;
+        }
+
+        line[i] = '\0';
+        if (*read == '\n') read++;
+
+        char *trim = line;
+        while (*trim == ' ' || *trim == '\t') trim++;
+
+        if (strncmp(trim, "define ", 7) == 0) {
+            parseDefine(trim);
+        } else {
+            strcpy(write, line);
+            write += strlen(line);
+            *write++ = '\n';
+        }
+    }
+
+    *write = '\0';
+}
+
+void applyDefines(char *str) {
+    size_t size = strlen(str);
+
+    char *buffer = malloc(size + defineCount * MAX_DEFINE_VALUE_LENGHT + 1);
+    char *write = buffer;
+    char *read = str;
+
+    while (*read) {
+        int replaced = 0;
+
+        for (int i = 0; i < defineCount; i++) {
+            int len = strlen(defines[i].name);
+
+            if (strncmp(read, defines[i].name, len) == 0) {
+                char before = (read == str) ? ' ' : read[-1];
+                char after = read[len];
+
+                if ((!isalnum(before) && before != '_') &&
+                    (!isalnum(after) && after != '_')) {
+
+                    strcpy(write, defines[i].value);
+                    write += strlen(defines[i].value);
+                    read += len;
+                    replaced = 1;
+                    break;
+                }
+            }
+        }
+
+        if (!replaced) {
+            *write++ = *read++;
+        }
+    }
+
+    *write = '\0';
+    strcpy(str, buffer);
+    free(buffer);
+}
+
+void removeCarriageReturn(char *str) {
+    char *read = str;
+    char *write = str;
+
+    while (*read) {
+        if (*read != '\r') *write++ = *read;
+        read++;
+    }
+
+    *write = '\0';
+}
+
+void removeComments(char* str) {
+    char *read = str;
+    char *write = str;
+
+    while (*read) {
+        if (*read == '/' && *(read + 1) == '/') {
+            while (*read && *read != '\n') read++;
+        } else {
+            *write = *read;
+            write++;
+            read++;
+        }
+    }
+
+    *write = '\0';
+}
+
+void removeEmptyLines(char *str) {
+    char *read = str;
+    char *write = str;
+
+    while (*read) {
+        char *lineStart = read;
+
+        while (*read == ' ' || *read == '\t') read++;
+
+        if (*read == '\n') {
+            read++;
+            continue;
+        }
+
+        read = lineStart;
+        while (*read && *read != '\n') {
+            *write++ = *read++;
+        }
+
+        if (*read == '\n') {
+            *write++ = *read++;
+        }
+    }
+
+    *write = '\0';
+}
 
 char* preprocess(const char* src, long srcSize, const char* filename) {
     char* out = malloc((srcSize + 1) * sizeof(char));
     memcpy(out, src, srcSize);
     out[srcSize] = '\0';
 
-    for (int i = 0; out[i] != '\0'; i++) {
-        
-    }
+    memset(defines, 0, sizeof(defines));
+    defineCount = 0;
+
+    removeCarriageReturn(out);
+    removeComments(out);
+    extractDefines(out);
+    applyDefines(out);
+    removeEmptyLines(out);
 
     return out;
 }
